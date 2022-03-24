9b7121c54ad1d88e189e59c4a37061f4ab1f3d3e
yhkim <neitotoro@naver.com>
2022-03-24 12:59:56 +0900

    first commit

---
+++ ecote/dfs_bfs.py
@@ -0,0 +1,29 @@
+from collections import deque
+stack = []
+stack.append(5)
+stack.append(2)
+stack.append(3)
+stack.append(7)
+stack.pop()
+stack.append(1)
+stack.append(4)
+stack.pop()
+
+print(stack[::-1])  # 거꾸로 출력
+print(stack)
+
+# 큐 구현을 위해 deque라이브러리를 사용
+queue = deque()
+
+queue.append(5)
+queue.append(2)
+queue.append(3)
+queue.append(7)
+queue.popleft()
+queue.append(1)
+queue.append(4)
+queue.popleft()
+
+print(queue)
+queue.reverse()
+print(queue)
